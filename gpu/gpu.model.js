3aeedefbdafd7af0d1a831e189508815fe968439
Nate Geslin <teamtomkins23@gmail.com>
2017-08-22 13:54:35 -0500

    adds index to GpuModel

--- gpu/gpu.model.js
+++ gpu/gpu.model.js
@@ -2,17 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const gpuSchema = Schema({
-    cardId: String,
+    cardId: { type: String, index: true, unique: true },
     manufacturer: String,
     model: String,
     memory: Number,
     rig: String,
-    rigPosition: Number,
-    mhs: Number,
-    coreVoltage: Number,
-    coreClock: Number,
-    memoryClock: Number,
+    rigPosition: { type: Number, default: -1 },
+    mhs: { type: Number, default: 10 },
+    coreVoltage: { type: Number, default: -1 },
+    coreClock: { type: Number, default: -1 },
+    memoryClock: { type: Number, default: -1 },
     cost: Number,
+    power: { type: Number, default: 100 },
     purchaseDate: Date
 }, { collection: 'gpus' });
 
